96af6172f8ea6551bbf3551ddcd2eecb3cbc4364
joychiu <joy.chiu@citiesocial.com>
2025-10-18 11:02:20 +0800

    chore: add Prettier configuration and update ESLint settings

--- eslint.config.js
+++ eslint.config.js
@@ -3,11 +3,9 @@ import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
-
-import prettierPlugin from "eslint-plugin-prettier";
-import configPrettier from "eslint-config-prettier";
-
 import { defineConfig, globalIgnores } from "eslint/config";
+
+import configPrettier from "eslint-config-prettier";
 
 export default defineConfig([
   globalIgnores(["dist"]),
@@ -15,26 +13,19 @@ export default defineConfig([
     files: ["**/*.{ts,tsx}"],
     extends: [
       js.configs.recommended,
-      tseslint.configs.recommended,
+      ...tseslint.configs.recommended,
       reactHooks.configs["recommended-latest"],
       reactRefresh.configs.vite,
-      // 關掉會與 Prettier 衝突的規則
-      { rules: configPrettier.rules },
+      configPrettier, // 關掉所有與 Prettier 衝突的規則
     ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
-    // 啟用 Prettier 作為 ESLint 規則來檢查格式
-    plugins: {
-      prettier: prettierPlugin,
-    },
 
     rules: {
-      // 不需要 React 17+ 的 React in scope
+      "react/react-in-jsx-scope": "off", //  React 17 開始不需要 import React
       "react-refresh/only-export-components": "off",
-      // Prettier 格式化檢查（違反時給警告，可自行調成 'error'）
-      "prettier/prettier": "warn",
     },
   },
 ]);
