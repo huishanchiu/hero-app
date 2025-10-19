ef91878fa26c4e982fa26542ce2dd315d61ec447
Joy.ch <84556615+huishanchiu@users.noreply.github.com>
2025-10-19 15:26:47 +0800

    Revert "feat: add API client and hero API functions with hooks for data fetching"

--- src/api/apiClient.ts
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export const apiClient = axios.create({
-  baseURL: "https://hahow-recruit.herokuapp.com",
-  headers: {
-    "Content-Type": "application/json",
-  },
-  timeout: 10000,
-});
